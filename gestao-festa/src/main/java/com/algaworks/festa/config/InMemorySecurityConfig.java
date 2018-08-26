89afbfea3cade100f32bc7a9c89aca8e95d774ba
Eberson Ramos <beer22k@gmail.com>
2018-08-26 20:00:14 -0300

    Alterado a versão do spring

--- gestao-festa/src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
+++ gestao-festa/src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
@@ -1,17 +1,37 @@
 package com.algaworks.festa.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class InMemorySecurityConfig {
-
+	
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder builder) {
+	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
 		
 		builder
-			.inMemoryAuthentication()
-			.withUser("joao");
+			.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+			.withUser("joao")
+				.password("$2a$10$HB.MxuJNKS.oR2WMNToAmeCxao/2haZkHJFqjfUp2K/R2PV7nEeBe").roles("USER")
+			.and()
+			.withUser("eberson")
+				.password("$2a$10$HB.MxuJNKS.oR2WMNToAmeCxao/2haZkHJFqjfUp2K/R2PV7nEeBe").roles("USER")
+			.and()
+			.withUser("maria")
+				.password("$2a$10$HB.MxuJNKS.oR2WMNToAmeCxao/2haZkHJFqjfUp2K/R2PV7nEeBe").roles("USER");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("123"));
 	}
 }
