df68d3ae15e3c2888df31796129e5939b87fc928
Eberson Ramos <beer22k@gmail.com>
2018-08-26 14:57:36 -0300

    Primeiro Commit

---
+++ gestao-festa/src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
@@ -0,0 +1,17 @@
+package com.algaworks.festa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+public class InMemorySecurityConfig {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder builder) {
+		
+		builder
+			.inMemoryAuthentication()
+			.withUser("joao");
+	}
+}
